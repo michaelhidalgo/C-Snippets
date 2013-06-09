6323dab8758e64c6bf0d7ebed664233bd66a3957
Michael <michaelfallas@gmail.com>
2013-06-09 06:49:53 -0600

    Adding the Assemblies Project to GH

---
+++ Assembly1/Person.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+
+namespace Assembly1
+{
+    public class Person
+    {
+        #region Properties
+
+        public List<Person> Persons;
+        protected internal string ID { get; set; }
+        internal int Age { get; set; }
+        public string FirstName { get; set; }
+        public string LasName { get; set; }
+        public string EmailAddress { get; set; }
+        public string HomePhone { get; set; }
+        public string WorkPhone { get; set; }
+        public string Address1 { get; set; }
+        public string Address2 { get; set; }
+
+        #endregion
+
+        #region Constructor
+
+        public Person()
+        {
+            Persons = new List<Person>();
+        }
+
+        #endregion
+
+        #region Methods
+
+        public void AddPerson(Person p)
+        {
+            Persons.Add(p);
+        }
+
+        public void RemovePerson(string Id)
+        {
+            Persons.RemoveAll(x => x.ID == Id);
+        }
+
+        #endregion
+    }
+}
---
+++ Assembly2/Employee.cs
@@ -0,0 +1,25 @@
+﻿using Assembly1;
+
+namespace Assembly2
+{
+    public class Employee : Person
+    {
+    }
+
+    public class Payment
+    {
+        public void ProcessPayment()
+        {
+            var employee = new Employee
+                {
+                    FirstName = "Michael",
+                    LasName = "Hidalgo",
+                    Address1 = "San Jose, Costa Rica",
+                    Address2 = "Street 27,Cartago",
+                    EmailAddress = "michaelfallas@gmail.com",
+                    HomePhone = "+506 83423557",
+                    WorkPhone = "+506 83423557"
+                };
+        }
+    }
+}
